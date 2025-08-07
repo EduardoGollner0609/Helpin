d8622d006cb1a406a476e4c64a46593465ceb871
Eduardo Sousa Gollner <eduardo.gollner@solutionsa.com.br>
2025-08-07 20:33:37 -0300

    Criando projeto backend

---
+++ backend/src/main/java/com/eduardo/helpin/HelpinApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.helpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eduardo/helpin/HelpinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.helpin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
