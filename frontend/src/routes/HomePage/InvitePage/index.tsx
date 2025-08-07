286a09bf064d32f8bb3bf3d52df5f8f8bb13491b
Eduardo Sousa Gollner <eduardo.gollner@solutionsa.com.br>
2025-08-07 20:49:40 -0300

    Adicionando react router dom ao projeto e configurando frontend.

---
+++ frontend/src/routes/HomePage/InvitePage/index.tsx
@@ -0,0 +1,9 @@
+export default function InvitePage() {
+    return (
+        <main>
+            <section id="invite-page-section">
+                <h1>Olá mundo</h1>
+            </section>
+        </main>
+    );
+}
